7a568def1a9fb4fc94a9d997dcb16a8e152fa905
jw29247 <jw29247@gmail.com>
2026-08-24 23:20:29 +0100

    release: WhiskerFlow 0.8.5 faster dictation and retention

--- Casks/whiskerflow.rb
+++ Casks/whiskerflow.rb
@@ -1,6 +1,6 @@
 cask "whiskerflow" do
   version "0.8.5"
-  sha256 "a2b664ef0a7b5117399da69b0d58186dcffcc3347463e5ec8aa78a807ffe0176"
+  sha256 "714c33d42af1fca409e05ba0680d70a0b11b397317ed4347ef79c233994cda0e"
 
   url "https://github.com/jw29247/WhiskerFlow/releases/download/v#{version}/WhiskerFlow-#{version}.dmg"
   name "WhiskerFlow"
